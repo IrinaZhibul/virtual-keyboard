e8c47167f7e375cddab8dcf5398b49527efaf64d
irinazhibul <zhibulirina@gmail.com>
2023-05-01 13:23:07 +0400

    feat: add switch language

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import { keysEn } from "./keys-en.js";
 import { keysBy } from "./keys-by.js";
 
-let currentLanguage = 'en';
-let keys = keysEn;
+
+let keys;
 let isCapsLock = false;
+
+const LEFT_SHIFT_KEY_CODE = "ShiftLeft";
+const RIGHT_SHIFT_KEY_CODE = "ShiftRight";
+const LEFT_CONTROL_KEY_CODE = "ControlLeft";
+const RIGHT_CONTROL_KEY_CODE = "ControlRight";
+let currentLanguage;
+
+initKeys();
 
 const container = document.createElement('div');
 container.classList = 'container';
@@ -12,57 +20,147 @@ textarea.classList = 'textarea';
 document.body.appendChild(container);
 container.appendChild(textarea);
 
-function switchLanguage() {
-    if (currentLanguage === 'en') {
-        currentLanguage = 'by';
-        keys = keysBy;
-    } else {
-        currentLanguage = 'en';
-        keys = keysEn;
-    }
+let buttons = keys.map((key) => {
+    const button = document.createElement('button');
+    button.classList = "btn";
+    button.style.width = key.width || '50px';
+    button.setAttribute('code', key.code)
+    button.textContent = key.mainKey;
+    container.appendChild(button);
+    return button;
+});
 
-    buttons.forEach((button, index) => {
-        button.textContent = keys[index];
-    });
-}
-
-function handleKeyPress(key) {
-    if (key.mainKey === 'Backspace') {
-        textarea.value = textarea.value.slice(0, -1);
-    } else if (key.mainKey === 'Del') {
-        textarea.value = textarea.value.slice(1);
-    } else if (key.mainKey === 'Tab') {
-        textarea.value += '\t';
-    } else if (key.mainKey === 'Enter') {
-        textarea.value += '\n';
-    } else if (key.mainKey === 'Space') {
-        textarea.value += ' ';
-    } else if (key.mainKey === 'Caps Lock') {
-        toggleCapsLock();
-    } else {
-        textarea.value += isCapsLock ? key.mainKey.toUpperCase() : key.mainKey;
-    }
-}
 
 document.addEventListener('keydown', (event) => {
-    console.log(event)
-    event.preventDefault();
-    let keyMapped = event.key == ' ' ? 'Space' : event.key;
-    const keyObj = keys.find(x => x.mainKey.replace(' ','').toLowerCase() == keyMapped.toLowerCase());
+    if (event.ctrlKey && event.shiftKey && (event.code === LEFT_SHIFT_KEY_CODE || event.code === LEFT_CONTROL_KEY_CODE)) {
+        changeLanguage();
+        return;
+    }
+
+    if (event.code != "ArrowUp" && event.code != "ArrowDown" && event.code != "ArrowLeft" && event.code != "ArrowRight") {
+        event.preventDefault();
+    }
+    const keyObj = keys.find(x => x.code.toLowerCase() == event.code.toLowerCase());
     console.log(keyObj)
     if (keyObj) {
         handleKeyPress(keyObj);
     }
 });
 
-const buttons = keys.map((key) => {
-    const button = document.createElement('button');
-    button.classList = "btn";
-    button.textContent = key.mainKey;
-    button.addEventListener('click', () => handleKeyPress(key));
-    container.appendChild(button);
-    return button;
-});
+
+subscribeOnKeyClickEvent();
+
+
+
+
+function initKeys() {
+    let storedLang = sessionStorage.getItem('lang') || 'en';
+    switch (storedLang) {
+        case 'en': keys = keysEn; currentLanguage = 'en'; break;
+        case 'by': keys = keysBy; currentLanguage = 'by'; break;
+        default: keys = keysEn; sessionStorage.setItem('lang', 'en'); currentLanguage = 'en'; break;
+    }
+}
+
+function changeLanguage() {
+    if (currentLanguage === 'en') {
+        currentLanguage = 'by';
+        keys = keysBy;
+
+    } else {
+        currentLanguage = 'en';
+        keys = keysEn;
+    }
+    sessionStorage.setItem('lang', currentLanguage)
+
+    if (buttons) {
+        buttons.forEach((button, index) => {
+            button.textContent = keys[index].mainKey;
+        });
+    }
+}
+
+function handleKeyPress(key) {
+    const ingoredKeys = [LEFT_SHIFT_KEY_CODE, RIGHT_SHIFT_KEY_CODE, RIGHT_CONTROL_KEY_CODE, LEFT_CONTROL_KEY_CODE, 'AltRight', 'AltLeft', 'MetaLeft']
+    if (ingoredKeys.includes(key.code)) {
+        return;
+    }
+    if (key.code === 'Backspace') {
+        textarea.value = textarea.value.slice(0, -1);
+    } else if (key.code === 'Delete') {
+        textarea.value = textarea.value.slice(1);
+    }
+    else if (key.code === 'ArrowDown') {
+        moveCursorDown(textarea);
+        textarea.focus();
+    }
+    else if (key.code === 'ArrowUp') {
+        moveCursorUp(textarea);
+        textarea.focus();
+    } 
+    else if (key.code === 'ArrowLeft') {
+        moveCursorLeft(textarea);
+        textarea.focus();
+    } 
+    else if (key.code === 'ArrowRight') {
+        moveCursorRight(textarea);
+        textarea.focus();
+    }     
+    else if (key.code === 'Tab') {
+        textarea.value += '\t';
+
+    } else if (key.code === 'Enter') {
+        textarea.value += '\n';
+    } else if (key.code === 'Space') {
+        textarea.value += ' ';
+    } else if (key.code === 'CapsLock') {
+        toggleCapsLock();
+    } else {
+        textarea.value += isCapsLock ? key.mainKey.toUpperCase() : key.mainKey;
+        textarea.focus();
+    }
+}
+function moveCursorDown(element) {
+    const currentPosition = element.selectionStart;
+    const lineBreakIndex = element.value.indexOf('\n', currentPosition);
+    if (lineBreakIndex === -1) {
+        element.selectionStart = element.selectionEnd = element.value.length;
+    
+    } else {
+        element.selectionStart = element.selectionEnd = lineBreakIndex + 1;
+    }
+}
+
+function moveCursorLeft(element) {
+    const currentPosition = element.selectionStart;
+    element.selectionStart = element.selectionEnd = currentPosition - 1;
+  }
+  function moveCursorRight(element) {
+    const currentPosition = element.selectionStart;
+    element.selectionStart = element.selectionEnd = currentPosition + 1;
+  }
+
+
+function moveCursorUp(element) {
+    const currentPosition = element.selectionStart;
+    const lineBreakIndex = element.value.lastIndexOf('\n', currentPosition - 1);
+    if (lineBreakIndex === -1) {
+      element.selectionStart = element.selectionEnd = 0;
+    } else {
+      element.selectionStart = element.selectionEnd = lineBreakIndex + 1;
+    }
+  }
+
+function subscribeOnKeyClickEvent() {
+    buttons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            event.preventDefault();
+            const keyObj = keys.find(x => x.code.toLowerCase() == event.target.getAttribute('code').toLowerCase());
+            handleKeyPress(keyObj);
+        });
+    });
+}
+
 
 function toggleCapsLock() {
     isCapsLock = !isCapsLock;
@@ -72,6 +170,7 @@ function toggleCapsLock() {
         }
     });
 }
+
 function isCaseSensetive(button) {
     let arr = [
         "Caps Lock",
